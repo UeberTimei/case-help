b4605068aed2a7e6601c0e6308a6f1c08d8b9bea
UeberTimei <super.timati04@gmail.com>
2024-07-08 14:38:16 +0300

    feat: Настройка проекта

---
+++ src/blocks/modules/results/results.js
@@ -0,0 +1,13 @@
+modules.define('results', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
